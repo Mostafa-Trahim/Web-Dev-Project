79981d7917761196a6ad21f0a8183135113da2d2
toshi-Devs <trahim87mustapha@gmail.com>
2024-03-14 23:05:12 +0200

    react init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
